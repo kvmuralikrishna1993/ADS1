420c6f8380f32c0e30cb852154b69be82c0a3150
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-10-02 16:18:51 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- assigments/m5/Josephus Problem/Queue.java
+++ assigments/m5/Josephus Problem/Queue.java
@@ -13,62 +13,62 @@ class EmptyException extends Exception {
     }
 }
 public class Queue<E> implements Iterable<E> {
-	private class Node {
-		E data;
-		Node next;
-	}
-	private Node head, tail;
-	private int size = 0;
+    private class Node {
+        E data;
+        Node next;
+    }
+    private Node head, tail;
+    private int size = 0;
 
-	public void enqueue(E data) {
-		Node node = new Node();
-		node.data = data;
-		size++;
-		if (tail == null) {
-			tail = node;
-			head = node;
-			return;
-		}
-		tail.next = node;
-		tail = tail.next;
-	}
+    public void enqueue(E data) {
+        Node node = new Node();
+        node.data = data;
+        size++;
+        if (tail == null) {
+            tail = node;
+            head = node;
+            return;
+        }
+        tail.next = node;
+        tail = tail.next;
+    }
 
-	public E dequeue() {
-			E data = head.data;
-			head = head.next;
-			size--;
-			return data;
-	}
+    public E dequeue() {
+            E data = head.data;
+            head = head.next;
+            size--;
+            return data;
+    }
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
-	public int size() {
-		return size;
-	}
-	public Iterator iterator() {
-		return new MyIterator(head);
-	}
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public int size() {
+        return size;
+    }
+    public Iterator iterator() {
+        return new MyIterator(head);
+    }
 
-	private class MyIterator implements Iterator {
-		Node current;
+    private class MyIterator implements Iterator {
+        Node current;
 
-		public MyIterator(Node first) {
-			current = first;
-		}
+        public MyIterator(Node first) {
+            current = first;
+        }
 
-		public boolean hasNext() {
-			return current !=  null;
-		}
+        public boolean hasNext() {
+            return current !=  null;
+        }
 
-		public void remove() {
+        public void remove() {
 
-		}
+        }
 
-		public E next() {
-			E data = current.data;
-			current = current.next;
-			return data;
-		}
-	}
+        public E next() {
+            E data = current.data;
+            current = current.next;
+            return data;
+        }
+    }
 }
--- assigments/m5/Josephus Problem/Solution.java
+++ assigments/m5/Josephus Problem/Solution.java
@@ -2,51 +2,43 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_main }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    }
+    /**
+     * { function_main }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
         while (sc.hasNext()) {
-        	Queue<Integer> q = new Queue<>();
-        	String input = sc.nextLine();
-        	String[] tokens = input.split(" ");
-        	for (int i = 0; i < Integer.parseInt(tokens[0]); i++) {
-				q.enqueue(i);
-			}
-			//System.out.println(q.size());
-			int k = Integer.parseInt(tokens[1]);
-			String str = "";
-			while (q.size() > 1) {
-				//------------------------------------------
-				for (int i = 0; i < k - 1; i++) {
-					//try {
-						q.enqueue(q.dequeue());
-					//} catch(Exception e) {
-						//break;
-					//}
-				}
-				//-------------------------------------------
-				//try {
-					System.out.print(q.dequeue() + " ");
-				//} catch(Exception e) {
-					//break;
-				//}
-				//-------------------------------------------
-			}
-			System.out.print(q.dequeue());
-			System.out.println();
+            Queue<Integer> q = new Queue<>();
+            String input = sc.nextLine();
+            String[] tokens = input.split(" ");
+            for (int i = 0; i < Integer.parseInt(tokens[0]); i++) {
+                q.enqueue(i);
+            }
+            //System.out.println(q.size());
+            int k = Integer.parseInt(tokens[1]);
+            String str = "";
+            while (q.size() > 1) {
+                //------------------------------------------
+                for (int i = 0; i < k - 1; i++) {
+                        q.enqueue(q.dequeue());
+                }
+                //-------------------------------------------
+                    System.out.print(q.dequeue() + " ");
+                //-------------------------------------------
+            }
+            System.out.print(q.dequeue());
+            System.out.println();
         }
-	}
+    }
 }
