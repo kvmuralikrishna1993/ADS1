0499cf3e10a36bce1b3b8f1909582d586c6a10a8
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-10-09 17:11:32 +0530

    "check_style_score -> 1 of 6 passed. style score: 1/1 "

--- assigments/m12(exam2)/Assignment-1/Insertion.java
+++ assigments/m12(exam2)/Assignment-1/Insertion.java
@@ -4,157 +4,226 @@ import java.util.Comparator;
  * Class for insertion.
  */
 class Insertion {
-
-    // This class should not be instantiated.
-    Insertion() { }
-
     /**
-     * Rearranges the array in ascending order, using the natural order.
-     * @param a the array to be sorted
+     * Constructs the object.
      */
-    public void sort(Comparable[] a) {
+    Insertion() { }
+    /**
+     * { function_sort }.
+     *
+     * @param      a     { parameter_description }
+     */
+    public void sort(final Comparable[] a) {
         int n = a.length;
         for (int i = 0; i < n; i++) {
-            for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
-                exch(a, j, j-1);
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
             }
             assert isSorted(a, 0, i);
         }
         assert isSorted(a);
     }
-
     /**
-     * Rearranges the subarray a[lo..hi) in ascending order, using the natural order.
-     * @param a the array to be sorted
-     * @param lo left endpoint (inclusive)
-     * @param hi right endpoint (exclusive)
+     * { function_sort }.
+     *
+     * @param      a     { parameter_description }
+     * @param      lo    The lower
+     * @param      hi    The higher
      */
-    public void sort(Comparable[] a, int lo, int hi) {
+    public void sort(final Comparable[] a, final int lo, final int hi) {
         for (int i = lo; i < hi; i++) {
-            for (int j = i; j > lo && less(a[j], a[j-1]); j--) {
-                exch(a, j, j-1);
+            for (int j = i; j > lo && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
             }
         }
         assert isSorted(a, lo, hi);
     }
-
     /**
-     * Rearranges the array in ascending order, using a comparator.
-     * @param a the array
-     * @param comparator the comparator specifying the order
+     * { function_sort }.
+     *
+     * @param      a           { parameter_description }
+     * @param      comparator  The comparator
      */
-    public void sort(Object[] a, Comparator comparator) {
+    public void sort(final Object[] a, final Comparator comparator) {
         int n = a.length;
         for (int i = 0; i < n; i++) {
-            for (int j = i; j > 0 && less(a[j], a[j-1], comparator); j--) {
-                exch(a, j, j-1);
+            for (int j = i; j > 0 && less(a[j], a[j - 1], comparator); j--) {
+                exch(a, j, j - 1);
             }
             assert isSorted(a, 0, i, comparator);
         }
         assert isSorted(a, comparator);
     }
-
     /**
-     * Rearranges the subarray a[lo..hi) in ascending order, using a comparator.
-     * @param a the array
-     * @param lo left endpoint (inclusive)
-     * @param hi right endpoint (exclusive)
-     * @param comparator the comparator specifying the order
+     * { function_sort}.
+     *
+     * @param      a           { parameter_description }
+     * @param      lo          The lower
+     * @param      hi          The higher
+     * @param      comparator  The comparator
      */
-    public void sort(Object[] a, int lo, int hi, Comparator comparator) {
+    public void sort(final Object[] a, final int lo, final int hi,
+        final Comparator comparator) {
         for (int i = lo; i < hi; i++) {
-            for (int j = i; j > lo && less(a[j], a[j-1], comparator); j--) {
-                exch(a, j, j-1);
+            for (int j = i; j > lo && less(a[j], a[j - 1], comparator); j--) {
+                exch(a, j, j - 1);
             }
         }
         assert isSorted(a, lo, hi, comparator);
     }
-
-
-    // return a permutation that gives the elements in a[] in ascending order
-    // do not change the original array a[]
     /**
-     * Returns a permutation that gives the elements in the array in ascending order.
-     * @param a the array
-     * @return a permutation {@code p[]} such that {@code a[p[0]]}, {@code a[p[1]]},
-     *    ..., {@code a[p[n-1]]} are in ascending order
+     * { function_sort}.
+     *
+     * @param      a     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
      */
-    public int[] indexSort(Comparable[] a) {
+    public int[] indexSort(final Comparable[] a) {
         int n = a.length;
         int[] index = new int[n];
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             index[i] = i;
+        }
 
-        for (int i = 0; i < n; i++)
-            for (int j = i; j > 0 && less(a[index[j]], a[index[j-1]]); j--)
-                exch(index, j, j-1);
-
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j > 0 && less(a[index[j]], a[index[j - 1]]); j--) {
+                exch(index, j, j - 1);
+            }
+        }
         return index;
     }
-
-   /***************************************************************************
-    *  Helper sorting functions.
-    ***************************************************************************/    
-    // is v < w ?
-    private boolean less(Comparable v, Comparable w) {
+    /**
+     * { function_less}.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private boolean less(final Comparable v, final Comparable w) {
         return v.compareTo(w) < 0;
     }
-
-    // is v < w ?
-    private boolean less(Object v, Object w, Comparator comparator) {
+    /**
+     * { function_less}.
+     *
+     * @param      v           { parameter_description }
+     * @param      w           { parameter_description }
+     * @param      comparator  The comparator
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private boolean less(final Object v, final Object w,
+        final Comparator comparator) {
         return comparator.compare(v, w) < 0;
     }
-        
-    // exchange a[i] and a[j]
-    private void exch(Object[] a, int i, int j) {
+    /**
+     * { function_exchange}.
+     *
+     * @param      a     { parameter_description }
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
+    private void exch(final Object[] a, final int i, final int j) {
         Object swap = a[i];
         a[i] = a[j];
         a[j] = swap;
     }
-
-    // exchange a[i] and a[j]  (for indirect sort)
-    private void exch(int[] a, int i, int j) {
+    /**
+     * { function_exchange}.
+     *
+     * @param      a     { parameter_description }
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
+    private void exch(final int[] a, final int i, final int j) {
         int swap = a[i];
         a[i] = a[j];
         a[j] = swap;
     }
-
-   /***************************************************************************
-    *  Check if array is sorted - useful for debugging.
-    ***************************************************************************/
-    private boolean isSorted(Comparable[] a) {
+    /**
+     * Determines if sorted.
+     *
+     * @param      a     { parameter_description }
+     *
+     * @return     True if sorted, False otherwise.
+     */
+    private boolean isSorted(final Comparable[] a) {
         return isSorted(a, 0, a.length);
     }
-
-    // is the array a[lo..hi) sorted
-    private boolean isSorted(Comparable[] a, int lo, int hi) {
-        for (int i = lo+1; i < hi; i++)
-            if (less(a[i], a[i-1])) return false;
+    /**
+     * Determines if sorted.
+     *
+     * @param      a     { parameter_description }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     *
+     * @return     True if sorted, False otherwise.
+     */
+    private boolean isSorted(final Comparable[] a, final int lo,
+        final int hi) {
+        for (int i = lo + 1; i < hi; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
         return true;
     }
-
-    private boolean isSorted(Object[] a, Comparator comparator) {
+    /**
+     * Determines if sorted.
+     *
+     * @param      a           { parameter_description }
+     * @param      comparator  The comparator
+     *
+     * @return     True if sorted, False otherwise.
+     */
+    private boolean isSorted(final Object[] a, final Comparator comparator) {
         return isSorted(a, 0, a.length, comparator);
     }
-
-    // is the array a[lo..hi) sorted
-    private boolean isSorted(Object[] a, int lo, int hi, Comparator comparator) {
-        for (int i = lo+1; i < hi; i++)
-            if (less(a[i], a[i-1], comparator)) return false;
+    /**
+     * Determines if sorted.
+     *
+     * @param      a           { parameter_description }
+     * @param      lo          The lower
+     * @param      hi          The higher
+     * @param      comparator  The comparator
+     *
+     * @return     True if sorted, False otherwise.
+     */
+    private boolean isSorted(final Object[] a, final int lo,
+        final int hi, final Comparator comparator) {
+        for (int i = lo + 1; i < hi; i++) {
+            if (less(a[i], a[i - 1], comparator)) {
+                return false;
+            }
+        }
         return true;
     }
-
-   // print array to standard output
-    public void show(Comparable[] a) {
+    /**
+     * { function_show }.
+     *
+     * @param      a     { parameter_description }
+     */
+    public void show(final Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]);
-            if (i < a.length - 1){
+            if (i < a.length - 1) {
                 System.out.println();
             }
         }
         System.out.println();
     }
-    public void seats(Comparable[] a, int oc, int bc, int sc, int st, int total) {
+    /**
+     * { function_selected seats}.
+     *
+     * @param      a      { parameter_description }
+     * @param      oc     { parameter_description }
+     * @param      bc     { parameter_description }
+     * @param      sc     The screen
+     * @param      st     { parameter_description }
+     * @param      total  The total
+     */
+    public void seats(final Comparable[] a, final int oc, final int bc,
+        final int sc, final int st, final int total) {
         System.out.println();
         for (int i = 0; i < oc; i++) {
             System.out.print(a[i]);
@@ -162,17 +231,20 @@ class Insertion {
                 System.out.println();
             }
         }
-        bc = oc + bc;
-        for (int i = oc; i < bc; i++) {
+        int bc1 = bc;
+        for (int i = oc; i < total; i++) {
             String str = a[i].toString();
             int val = str.indexOf("BC");
             if (val > 0) {
                 System.out.println(a[i]);
+                bc1--;
+                if (bc1 == 0) {
+                    break;
+                }
             }
         }
-        int st1 = st; 
-        st = st+bc+oc;
-        for (int i = oc; i < st; i++) {
+        int st1 = st;
+        for (int i = oc; i < total; i++) {
             String str = a[i].toString();
             int val = str.indexOf("ST");
             if (val > 0) {
@@ -184,8 +256,7 @@ class Insertion {
             }
         }
         int sc1 = sc;
-        sc = total;
-        for (int i = oc; i < sc; i++) {
+        for (int i = oc; i < total; i++) {
             String str = a[i].toString();
             int val = str.indexOf("SC");
             if (val > 0) {
--- assigments/m12(exam2)/Assignment-1/Reservation.java
+++ assigments/m12(exam2)/Assignment-1/Reservation.java
@@ -1,64 +1,64 @@
-import java.util.Comparator;
 /**
  * Class for reservation.
  */
-class Reservation implements Comparable<Reservation>{
-	/**
-	 * { var_name }.
-	 */
-	private String name;
-	/**
-	 * { var_date }.
-	 */
-	private long days;
-	/**
-	 * { var_subject1 }.
-	 */
-	private String sub1;
-	/**
-	 * { var_subject2 }.
-	 */
-	private String sub2;
-	/**
-	 * { var_subject3 }.
-	 */
-	private String sub3;
-	/**
-	 * { var_totalsub }.
-	 */
-	private String totalsub;
-	/**
-	 * { var_reserv }.
-	 */
-	private String reserv;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      name      The name
-	 * @param      date      The date
-	 * @param      sub1      The sub 1
-	 * @param      sub2      The sub 2
-	 * @param      sub3      The sub 3
-	 * @param      totalsub  The totalsub
-	 * @param      reserv    The reserv
-	 */
-	Reservation(String name, long days, String sub1,
-		String sub2, String sub3, String totalsub, String reserv) {
-		this.name = name;
-		this.days = days;
-		this.sub1 = sub1;
-		this.sub2 = sub2;
-		this.sub3 = sub3;
-		this.totalsub = totalsub;
-		this.reserv = reserv;
-	}
-	/**
+class Reservation implements Comparable<Reservation> {
+    /**
+     * { var_name }.
+     */
+    private String name;
+    /**
+     * { var_date }.
+     */
+    private long days;
+    /**
+     * { var_subject1 }.
+     */
+    private String sub1;
+    /**
+     * { var_subject2 }.
+     */
+    private String sub2;
+    /**
+     * { var_subject3 }.
+     */
+    private String sub3;
+    /**
+     * { var_totalsub }.
+     */
+    private String totalsub;
+    /**
+     * { var_reserv }.
+     */
+    private String reserv;
+    /**
+     * Constructs the object.
+     *
+     * @param      name1      The name 1
+     * @param      days1      The days 1
+     * @param      sub11      The sub 11
+     * @param      sub21      The sub 21
+     * @param      sub31      The sub 31
+     * @param      totalsub1  The totalsub 1
+     * @param      reserv1    The reserv 1
+     */
+    Reservation(final String name1, final long days1, final String sub11,
+        final String sub21, final String sub31, final String totalsub1,
+        final String reserv1) {
+        this.name = name1;
+        this.days = days1;
+        this.sub1 = sub11;
+        this.sub2 = sub21;
+        this.sub3 = sub31;
+        this.totalsub = totalsub1;
+        this.reserv = reserv1;
+    }
+    /**
      * { get_name}.
      *
      * @return     { name}.
      */
     public String toString() {
-        return this.name+","+this.totalsub+","+this.reserv;
+        return this.name + "," + this.totalsub + "," + this.reserv;
     }
     /**
      * { function_compare }.
@@ -67,50 +67,61 @@ class Reservation implements Comparable<Reservation>{
      *
      * @return     { description_of_the_return_value }
      */
-	public int compareTo(Reservation that) {
-		if (Integer.parseInt(this.totalsub) > Integer.parseInt(that.totalsub)) {
+    public int compareTo(final Reservation that) {
+        if (Integer.parseInt(this.totalsub) > Integer.parseInt(that.totalsub)) {
             return -1;
-        } else if (Integer.parseInt(this.totalsub) < Integer.parseInt(that.totalsub)) {
+        } else if (Integer.parseInt(this.totalsub)
+            < Integer.parseInt(that.totalsub)) {
             return 1;
         } else if (Integer.parseInt(this.sub3) > Integer.parseInt(that.sub3)) {
             return -1;
-        } else if (Integer.parseInt(this.sub3) < Integer.parseInt(that.sub2)){
+        } else if (Integer.parseInt(this.sub3) < Integer.parseInt(that.sub2)) {
             return 1;
         } else if (Integer.parseInt(this.sub2) > Integer.parseInt(that.sub2)) {
             return -1;
-        } else if (Integer.parseInt(this.sub2) < Integer.parseInt(that.sub2)){
+        } else if (Integer.parseInt(this.sub2) < Integer.parseInt(that.sub2)) {
             return 1;
         } else if (this.days < that.days) {
             return -1;
         } else if (this.days > that.days) {
             return 1;
-        }  else if (this.reserv.equals("ST") || this.reserv.equals("SC") ||
-        	this.reserv.equals("BC") && that.reserv.equals("ST")
-        	|| that.reserv.equals("SC") || that.reserv.equals("BC")) {
-        	int res1 = reser(this.reserv);
-        	int res2 = reser(that.reserv);
-        	if (res1 > res2) {
-        		return -1;
-        	} else {
-        		return 1;
-        	}
+        }  else if (this.reserv.equals("ST") || this.reserv.equals("SC")
+            || this.reserv.equals("BC") && that.reserv.equals("ST")
+            || that.reserv.equals("SC") || that.reserv.equals("BC")) {
+            int res1 = reser(this.reserv);
+            int res2 = reser(that.reserv);
+            if (res1 > res2) {
+                return -1;
+            } else {
+                return 1;
+            }
         } else {
           return 0;
         }
     }
-    public int reser(String res) {
-    	int re = 0;
-    	if (res.equals("ST")){
-    		re = 3;
-    		return re;
-    	} else if (res.equals("SC")){
-    		re = 2;
-    		return re;
-    	} else if (res.equals("BC")){
-    		re = 1;
-    		return re;
-    	} else {
-    		return re;
-    	}
+    /**
+     * { function_reser}.
+     *
+     * @param      res   The resource
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int reser(final String res) {
+        int re = 0;
+        final int three = 3;
+        final int two = 2;
+        final int one = 1;
+        if (res.equals("ST")) {
+            re = three;
+            return re;
+        } else if (res.equals("SC")) {
+            re = two;
+            return re;
+        } else if (res.equals("BC")) {
+            re = one;
+            return re;
+        } else {
+            return re;
+        }
     }
 }
