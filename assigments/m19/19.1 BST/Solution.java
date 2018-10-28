65f6d8265d2cdddc369a6b88e50aa46b58689216
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-10-29 01:53:54 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assigments/m19/19.1 BST/Solution.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+/**
+ * solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_main }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        final int three = 3;
+        final int four = 4;
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTree b = new BinarySearchTree();
+        while (sc.hasNextLine()) {
+            String[] tokens = sc.nextLine().split(",");
+            switch (tokens[0]) {
+            case "put":
+                b.put(new Book(tokens[1], tokens[2],
+                                 Double.parseDouble(tokens[three])),
+                        Integer.parseInt(tokens[four]));
+                break;
+            case "get":
+                System.out.println(b.get(new Book(tokens[1],
+                    tokens[2], Double.parseDouble(tokens[three]))));
+                break;
+            case "max":
+                System.out.println(b.max());
+                break;
+            case "min":
+                System.out.println(b.min());
+                break;
+            case "floor":
+                System.out.println(b.floor(new Book(tokens[1],
+                    tokens[2], Double.parseDouble(tokens[three]))));
+                break;
+            case "ceiling":
+                System.out.println(b.ceiling(new Book(tokens[1],
+                    tokens[2], Double.parseDouble(tokens[three]))));
+                break;
+            case "select":
+                System.out.println(b.select(Integer.parseInt(tokens[1])));
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
